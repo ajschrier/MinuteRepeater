71fae0161c6e9744edc54ab8455240095173e598
AJ Schrier <aschrier@apple.com>
2016-09-14 17:01:03 -0700

    Python version: flake8 edits

--- python/minuterepeater.py
+++ python/minuterepeater.py
@@ -5,37 +5,41 @@ AJ Schrier
 import time
 
 # Get time
-hour=time.strftime("%I")
-minute=time.strftime("%M")
+hour = time.strftime("%I")
+minute = time.strftime("%M")
 
 # Chime the hours
 # One per hour, with a little pause in between
-def HourChime(currenthour):
-	print
-	for i in range(currenthour):
-		print "DONG"
-		time.sleep(0.5)
-	time.sleep(0.2)
 
-# Chime the minutes 
+
+def HourChime(currenthour):
+    print
+    for i in range(currenthour):
+        print "DONG"
+        time.sleep(0.5)
+    time.sleep(0.2)
+
+# Chime the minutes
 # One 'DING-DONG' per 15 minutes, and then a 'DING' for each remaining minute
+
+
 def MinuteChime(currentminute):
-	print
-	if currentminute==0:
-		return
-	else:
-		a=currentminute/15
-		b=currentminute%15
-	if a!=0:
-		for i in range(a):
-			print "DING-DONG"
-			time.sleep(0.5)
-		time.sleep(0.1)
-	if b!=0:
-		print
-		for i in range(b):
-			print "DING"
-			time.sleep(0.5)
+    print
+    if currentminute == 0:
+        return
+    else:
+        a = currentminute / 15
+        b = currentminute % 15
+    if a != 0:
+        for i in range(a):
+            print "DING-DONG"
+            time.sleep(0.5)
+        time.sleep(0.1)
+    if b != 0:
+        print
+        for i in range(b):
+            print "DING"
+            time.sleep(0.5)
 
 HourChime(int(hour))
 MinuteChime(int(minute))
