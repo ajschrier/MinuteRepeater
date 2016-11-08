4a9d76d26e02fca9499a8211c0b66b867d24bae1
AJ Schrier <ajschrier@AJs-MBP-13.attlocal.net>
2016-11-07 21:02:31 -0800

    Created TCTime

---
+++ TimeChimeObjC/TimeChimeObjC/TCTime.h
@@ -0,0 +1,13 @@
+//
+//  TCTime.h
+//  TimeChimeObjC
+//
+//  Created by AJ Schrier on 11/6/16.
+//  Copyright © 2016 AJ Schrier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCTime : NSObject
+
+@end
---
+++ TimeChimeObjC/TimeChimeObjC/TCTime.m
@@ -0,0 +1,13 @@
+//
+//  TCTime.m
+//  TimeChimeObjC
+//
+//  Created by AJ Schrier on 11/6/16.
+//  Copyright © 2016 AJ Schrier. All rights reserved.
+//
+
+#import "TCTime.h"
+
+@implementation TCTime
+
+@end
